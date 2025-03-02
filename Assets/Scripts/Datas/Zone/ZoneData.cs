d17e9627fa4704739e3b9d1f5fc904672bc46482
DeryaDincer <senaderyadincer@gmail.com>
2025-03-02 22:00:55 +0300

    Add title to ZoneData

--- Assets/Scripts/Datas/Zone/ZoneData.cs
+++ Assets/Scripts/Datas/Zone/ZoneData.cs
@@ -8,19 +8,17 @@ using VertigoGames.Datas.Reward;
 public class ZoneData : ScriptableObject
 {
     public ZoneType ZoneType => zoneType;
+    public int ZoneActivationInterval => _zoneActivationInterval;
     public List<RewardData> GuaranteedRewards => _guaranteedRewards;
     public List<RewardData> RandomRewards=> _randomRewards;
     public RewardWeight RewardWeight=> _rewardWeight;
     
+    [Title("Zone Settings")]
     [SerializeField] private ZoneType zoneType;
+    [SerializeField] private int _zoneActivationInterval;
+    
+    [Title("Rewards")]
     [SerializeField] private List<RewardData> _guaranteedRewards;
     [SerializeField] private List<RewardData> _randomRewards;
     [SerializeField] private RewardWeight _rewardWeight;
-}
-
-[Serializable]
-public class RewardWeightEntry
-{
-    public RewardWeight RewardWeightKey;
-    public float Value;
 }
