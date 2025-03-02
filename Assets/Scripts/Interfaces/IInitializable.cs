40bf84a97c9fdb6d86272fca74b614c08ef2973d
DeryaDincer <senaderyadincer@gmail.com>
2025-03-02 18:14:28 +0300

    IInitializable interface added

---
+++ Assets/Scripts/Interfaces/IInitializable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VertigoGames.Interfaces
+{
+    public interface IInitializable
+    {
+        void Initialize();    
+        void Deinitialize(); 
+    }
+}
